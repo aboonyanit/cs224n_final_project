fbc75d4cfd5c03fa4b7c38be136d4dfe9db8dff6
Anna Boonyanit <annaboon@stanford.edu>
2021-02-20 13:51:26 -0800

    Read dataframe

--- data_processing.py
+++ data_processing.py
@@ -5,6 +5,15 @@ import os
 # Input data files are available in the read-only "../input/" directory
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 
-for dirname, _, filenames in os.walk('/kaggle/input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
+# for dirname, _, filenames in os.walk('/kaggle/input'):
+#     for filename in filenames:
+#         print(os.path.join(dirname, filename))
+
+df_artists = pd.read_csv('../artists-data.csv')
+df_artists.head()
+
+df_artists_2c = df_artists[['Link', 'Genre']]
+df_artists_2c.head()
+
+
+
