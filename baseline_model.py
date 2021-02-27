445819930c3fdf81b16aab411eca52d537e06c88
Anna Boonyanit <annaboon@stanford.edu>
2021-02-27 15:43:15 -0800

    Add confusion matrix plotting, average GloVe and lyric count info

--- baseline_model.py
+++ baseline_model.py
@@ -5,7 +5,11 @@ import csv
 import pandas as pd
 import statistics
 from typing import List
+from torch.autograd import Variable
 import torch.utils.data as data_utils
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
 # This file creates and runs a Logistic Regression model. It contains methods necessary to generate
 # embeddings and add necessary padding. Small amounts of the code was based off of Assignment 4.
@@ -14,13 +18,11 @@ import torch.utils.data as data_utils
 def generate_embeddings(file_path):
     """ Generates an embeddings vector which contains the pre-trained GloVe vectors we generated.
     It also generates a list of the vocabulary.
-
     @param file_path (str): File path containing the GloVe vectors
-
     @returns vocab: list of vocab (number of vocab words)
     @returns embeddings: matrix of embeddings (vocabulary size, embedding dimension)
     """
-    embeddings_dict = {} # each key is word and value is vector of floats - not currently using it - can probably delete later
+    embeddings_dict = {} # each key is word and value is vector of floats
     embeddings = []
     vocab = []
     with open(file_path, "r") as f:
@@ -30,18 +32,36 @@ def generate_embeddings(file_path):
             embeddings_dict[split_line[0]] = vector
             embeddings.append(vector)
             vocab.append(split_line[0])
-            #Add unk token to dictionary
     vocab.append('<pad>')
-    embeddings.append([0] * 100) # Currently made the pad token 100 0's but can change that
-    return vocab, embeddings
+    embeddings.append([0] * 100)
+    embeddings_dict['<pad>'] = [0] * 100
+    return vocab, embeddings, embeddings_dict
+
+def to_average_input_tensor(self, lyrics_list: List[List[str]], device: torch.device) -> torch.Tensor:
+    """ Convert list of sentences (words) into tensor made with average embeddings.
+    @param lyrics_list (List[List[str]]): list of lyrics (words)
+    @param device: device on which to load the tesnor, i.e. CPU or GPU
+    @returns lyrics_var: tensor of (100, batch_size)
+    """
+    lyrics_var = []
+    for lyrics in lyrics_list:
+        lyrics_indicies = [0] * 100
+        for i, word in enumerate(lyrics):
+            if word not in self.word2indicies.keys():
+                lyrics_indicies = [x + y for x, y in zip(lyrics_indicies, self.embeddings_dict['<unk>'])]
+            else:
+                lyrics_indicies = [x + y for x, y in zip(lyrics_indicies, self.embeddings_dict[word])]
+        if i != 0:
+            lyrics_indicies = [x / i for x in lyrics_indicies]
+        lyrics_var.append(lyrics_indicies)
+    lyrics_var = torch.tensor(lyrics_var, dtype=torch.float, device=device)
+    return lyrics_var
     
 def to_input_tensor(self, lyrics_list: List[List[str]], device: torch.device) -> torch.Tensor:
     """ Convert list of sentences (words) into tensor with necessary padding for 
     shorter sentences.
-
     @param lyrics_list (List[List[str]]): list of lyrics (words)
     @param device: device on which to load the tesnor, i.e. CPU or GPU
-
     @returns lyrics_var: tensor of (longest_lyric_len, batch_size)
     """
     lyrics_var = []
@@ -61,16 +81,13 @@ def to_input_tensor(self, lyrics_list: List[List[str]], device: torch.device) ->
         lyrics_indicies += ([self.word2indicies['<pad>']] * num_pads_to_add)
         lyrics_var.append(lyrics_indicies)
     lyrics_var = torch.tensor(lyrics_var, dtype=torch.float, device=device)
-    print(lyrics_var.shape)
     return lyrics_var
-    # return torch.t(lyrics_var) - this was used in a4 idk why
 
 class LogisticRegression(nn.Module):
     """ Simple Logistic Regression Model
     """
-    def __init__(self, vocab_size, embedding_dim, embeddings, vocab, n_classes=3):
+    def __init__(self, vocab_size, embedding_dim, embeddings, vocab, embeddings_dict, n_classes=3):
         """ Init LogisticRegression Model.
-
         @param embed_size (int): Embedding size (dimensionality)
         @param hidden_size (int): Hidden Size, the size of hidden states (dimensionality)
         @param vocab (Vocab): Vocabulary object containing src and tgt languages
@@ -78,11 +95,12 @@ class LogisticRegression(nn.Module):
         @param dropout_rate (float): Dropout probability, for attention
         """
         super(LogisticRegression, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_dim).from_pretrained(torch.FloatTensor(embeddings))
+        self.embedding = nn.Embedding(vocab_size, embedding_dim).from_pretrained(torch.FloatTensor(embeddings)) # Use this for non average tensor
+        self.embeddings_dict = embeddings_dict
         self.word2indicies = {word: ind for ind, word in enumerate(vocab)}
-        self.linear = nn.Linear(500, n_classes) #length of a padded sentence? - 
-        # self.linear = nn.Linear(4571, n_classes) #length of a padded sentence? - 
-        # 4571 is longest lyrics
+        #self.linear = nn.Linear(100, n_classes) # Use this for average tensor
+        self.linear = nn.Linear(500, n_classes) # Use this padding sentence up to 500 tokens
+        # 4571 is length of longest lyrics
         self.device = torch.device("cpu")
 
     def forward(self, lyrics:torch.LongTensor ) -> torch.Tensor:#List[List[str]]
@@ -92,10 +110,10 @@ class LogisticRegression(nn.Module):
         return self.linear(lyrics)
 
 if __name__ == '__main__':
-    vocab, embeddings = generate_embeddings('vectors.txt')
+    vocab, embeddings, embeddings_dict = generate_embeddings('vectors.txt')
     #create model
     device = torch.device('cpu')
-    model = LogisticRegression(len(vocab), len(embeddings[0]), embeddings, vocab)
+    model = LogisticRegression(len(vocab), len(embeddings[0]), embeddings, vocab, embeddings_dict)
     model = model.to(device)
     # get data
     testCSV = pd.read_csv("../cs224n_dataset/test-data.csv")
@@ -109,8 +127,6 @@ if __name__ == '__main__':
         y_train_csv.append([row["Hip Hop"], row["Pop"], row["Rock"]])
     for index, row in testCSV.iterrows():
         y_test_csv.append([row["Hip Hop"], row["Pop"], row["Rock"]])
-    # y_test_csv = [float(i) for i in testCSV["label"]]
-    # y_train_csv = [float(i) for i in trainCSV["label"]]
 
     x_test = torch.FloatTensor(to_input_tensor(model, lyrics_list = x_test_csv, device=device))
     x_train = torch.FloatTensor(to_input_tensor(model, lyrics_list =x_train_csv, device=device))    
@@ -118,34 +134,50 @@ if __name__ == '__main__':
     y_train =  torch.Tensor(y_train_csv)
 
     train_dataset = data_utils.TensorDataset(x_train, y_train)
-    train_loader = data_utils.DataLoader(train_dataset, batch_size=64, shuffle=True)
+    train_loader = data_utils.DataLoader(train_dataset, batch_size=128, shuffle=True)
     
     #hyperparameters
-    learning_rate = 0.001
-    epochs = 20
+    learning_rate = 0.00001
+    epochs = 200
 
-    # criterion = torch.nn.CrossEntropyLoss()
-    criterion = nn.MSELoss(size_average=False)
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
     #train model
+    print("Training model...")
     model.train()
     for epoch in range(epochs):
         for batch_idx, (data, targets) in enumerate(train_loader):
             data = data.to(device='cpu')
-            targets = targets.to(device='cpu')
+            targets = torch.argmax(targets, 1)
             y_pred = model(data)
-            # print(targets)
-            # print(y_pred)
-            # loss = criterion(y_pred, torch.max(targets, 1)[1]) - use this line to use crossentropyloss
             loss = criterion(y_pred, targets)
-            print("loss", loss)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+        print("loss", loss.item())
 
     #test model 
+    print("Testing model...")
     eval_y_pred = model(x_test)
+    eval_y_pred = torch.argmax(eval_y_pred, 1)
+    target = torch.argmax(y_test, 1)
+    accuracy = ((eval_y_pred.data == target.data).float().mean())    
+    print("Test accuracy: ", accuracy.item())
+    
+    nb_classes = 3
+    confusion_matrix = torch.zeros(nb_classes, nb_classes)
+    index = 0
+    with torch.no_grad():
+        for t, p in zip(target.view(-1), eval_y_pred.view(-1)):
+            # if t.item() != p.item():
+                # print("Target: ", t) #Use next 3 lines to print out example predictions
+                # print("Prediction: ", p)
+                # print(testCSV["Lyric"][index])
+            confusion_matrix[t.long(), p.long()] += 1
+            index += 1
 
-
-
+    confusion_matrix_df = pd.DataFrame(confusion_matrix, index=['Hip Hop', 'Pop', 'Rock'], columns=['Hip Hop', 'Pop', 'Rock']).astype("float")
+    sns.heatmap(confusion_matrix_df, annot=True, fmt='g')
+    plt.show()
+    print("Per class accuracy", confusion_matrix.diag() / confusion_matrix.sum(1))
--- generate_top_words.py
+++ generate_top_words.py
@@ -4,6 +4,35 @@ import pandas as pd
 import csv
 import matplotlib.pyplot as plt
 
+num_hip_hop = 0
+sum_hip_hop = 0
+num_pop = 0
+sum_pop = 0
+num_rock = 0
+sum_rock = 0
+num_lyrics = 0
+sum_lyrics = 0
+with open('../cs224n_dataset/lyric-genre-data.csv', 'r') as read_obj:
+    # Get average song length of different genres
+    csv_dict_reader = csv.DictReader(read_obj)
+    for row in csv_dict_reader:
+        lyric_len = len(row["Lyric"].split(' '))
+        sum_lyrics += lyric_len
+        num_lyrics += 1
+        if row["Hip Hop"] == '1':
+            num_hip_hop += 1
+            sum_hip_hop += lyric_len
+        elif row["Pop"] == '1':
+            num_pop += 1
+            sum_pop += lyric_len
+        elif row["Rock"] == '1':
+            num_rock += 1
+            sum_rock += lyric_len
+print("Avg song length", sum_lyrics / num_lyrics)
+print("Avg hip hop length", sum_hip_hop / num_hip_hop)
+print("Avg pop length", sum_pop / num_pop)
+print("Avg rock length", sum_rock / num_rock)
+
 rock_words = []
 pop_words = []
 hiphop_words = []
@@ -18,7 +47,6 @@ with open('../cs224n_dataset/lyric-genre-data-punctuation-separated.csv', 'r') a
             rock_words += lyrics
         else:
             hiphop_words += lyrics
-# print(rock_words)
 rock_words_dict = Counter(rock_words)
 pop_words_dict = Counter(pop_words)
 hiphop_words_dict = Counter(hiphop_words)
@@ -38,7 +66,6 @@ for word_pair in rock_words_dict.most_common(300):
         hiphop_words.append(word_pair[0])
         hiphop_counts.append(word_pair[1])
 fig = plt.figure()
-# ax = fig.add_axes([0,0,1,1])
 print(hiphop_words)
 print(hiphop_counts)
 print(len(hiphop_words))
@@ -55,7 +82,6 @@ for word_pair in pop_words_dict.most_common(300):
         pop_counts.append(word_pair[1])
 
 fig = plt.figure()
-# ax = fig.add_axes([0,0,1,1])
 print(pop_words)
 print(pop_counts)
 print(len(pop_words))
@@ -72,7 +98,6 @@ for word_pair in hiphop_words_dict.most_common(300):
         hiphop_counts.append(word_pair[1])
 
 fig = plt.figure()
-# ax = fig.add_axes([0,0,1,1])
 print(hiphop_words)
 print(hiphop_counts)
 print(len(hiphop_words))
