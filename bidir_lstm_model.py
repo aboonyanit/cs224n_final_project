938beed566df0012906937fc577d34592659ecf1
Andrea Dahl <ahdahl@stanford.edu>
2021-03-03 10:59:10 -0800

    separate lstm and bidir files

--- bidir_lstm_model.py
+++ bidir_lstm_model.py
@@ -21,7 +21,9 @@ import matplotlib.pyplot as plt
 def generate_embeddings(file_path):
     """ Generates an embeddings vector which contains the pre-trained GloVe vectors we generated.
     It also generates a list of the vocabulary.
+
     @param file_path (str): File path containing the GloVe vectors
+
     @returns vocab: list of vocab (number of vocab words)
     @returns embeddings: matrix of embeddings (vocabulary size, embedding dimension)
     """
@@ -43,8 +45,10 @@ def generate_embeddings(file_path):
 def to_input_tensor(self, lyrics_list: List[List[str]], max_len_padded_seq, device) -> torch.Tensor:
     """ Convert list of sentences (words) into tensor with necessary padding for 
     shorter sentences.
+
     @param lyrics_list (List[List[str]]): list of lyrics (words)
     @param device: device on which to load the tesnor, i.e. CPU or GPU
+
     @returns lyrics_var: tensor of (longest_lyric_len, batch_size)
     """
     lyrics_var = []
@@ -121,6 +125,7 @@ class LSTM_model(nn.Module):
     """
     def __init__(self, vocab_size, embedding_dim, embeddings, vocab, hidden_dim, num_layers, n_classes=3):
         """ Init LSTM_model Model.
+
         @param embed_size (int): Embedding size (dimensionality)
         @param hidden_size (int): Hidden Size, the size of hidden states (dimensionality)
         @param vocab (Vocab): Vocabulary object containing src and tgt languages
@@ -130,19 +135,32 @@ class LSTM_model(nn.Module):
         super(LSTM_model, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=0).from_pretrained(torch.FloatTensor(embeddings))
         self.word2indicies = {word: ind for ind, word in enumerate(vocab)}
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers, batch_first=True)
-        self.linear = nn.Linear(hidden_dim, n_classes) #changed this from 5 to n_classes
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers, batch_first=True, bidirectional=True)
+        # self.lstm2 = nn.LSTM(input_size=hidden_dim*2,
+        #                     hidden_size=hidden_dim,
+        #                     num_layers=1, 
+        #                     bidirectional=True)
+        self.linear = nn.Linear(hidden_dim * 2, n_classes)
         self.dropout = nn.Dropout(0.2)
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.num_layers = num_layers
+        self.hidden_dim = hidden_dim
 
 
     def forward(self, lyrics:torch.LongTensor, totalLen ) -> torch.Tensor:#List[List[str]]
         # Convert list of lists into tensors
+
         x = self.embedding(lyrics)
         x = self.dropout(x)
         x_pack = pack_padded_sequence(x, totalLen, batch_first=True, enforce_sorted=False)
-        out_pack, (hidden_state, cell_state) = self.lstm(x_pack)
+        h0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_dim).to(device) # 2 for bidirection 
+        c0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_dim).to(device)
+        out_pack, (hidden_state, cell_state) = self.lstm(x_pack, (h0, c0))
+        output_unpacked, output_lengths = pad_packed_sequence(out_pack, batch_first=True)
+        # out_pack, (hidden_state, cell_state) = self.lstm2(x)
+
         out = self.linear(hidden_state[-1]).to(device)
+        #out = self.linear(output_unpacked[:, -1, :]).to(device)
         return out
 
 if __name__ == '__main__':
